4ce23eb08a53b90a8af84b715de22956e63d3b43
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-02-28 07:52:03 +0100

    [Feature] Include new classes
    This is the initial commit from local and include the following.
    - Composer configuration details are added setup with composer.json
    - Coverage settings with TravisCI and CircleCI are added
    - Testing configurations are include with the phpunit.xml file.
    Additionally new class files are included in addition to new testw class files.
    
    This is the first commit to start the Open Source Evangelist Project.

---
+++ src/Evangelist.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Elchroy\OSE;
+
+
+// require '../vendor/autoload.php';
+use GuzzleHttp\Client;
+use Elchroy\OSE\NoMethodException;
+
+/**
+ * This is the Envagelist Class of GitHub.com users. It manages and ranks users based on certain criteria
+ *
+ */
+class Evangelist
+{
+    public $user;
+
+    public $ranks = array(
+            1 => "Junior Evangelist",
+            2 => "Associate Evanlegist",
+            3 => "Senior Evangelist",
+        );
+    public $repos;
+
+    public $userData = '{
+  "login": "unicodeveloper",
+  "id": 2946769,
+  "avatar_url": "https://avatars.githubusercontent.com/u/2946769?v=3",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/unicodeveloper",
+  "html_url": "https://github.com/unicodeveloper",
+  "followers_url": "https://api.github.com/users/unicodeveloper/followers",
+  "following_url": "https://api.github.com/users/unicodeveloper/following{/other_user}",
+  "gists_url": "https://api.github.com/users/unicodeveloper/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/unicodeveloper/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/unicodeveloper/subscriptions",
+  "organizations_url": "https://api.github.com/users/unicodeveloper/orgs",
+  "repos_url": "https://api.github.com/users/unicodeveloper/repos",
+  "events_url": "https://api.github.com/users/unicodeveloper/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/unicodeveloper/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Prosper Otemuyiwa",
+  "company": "Andela",
+  "blog": "http://goodheads.io",
+  "location": "Lagos, Nigeria",
+  "email": "prosperotemuyiwa@gmail.com",
+  "hireable": null,
+  "bio": null,
+  "public_repos": 62,
+  "public_gists": 5,
+  "followers": 138,
+  "following": 38,
+  "created_at": "2012-12-02T21:53:16Z",
+  "updated_at": "2016-02-26T17:31:43Z"
+}';
+
+    public function __construct($username)
+    {
+        $client = new Client;
+        // $client->request('GET', "https://api.github.com/users/$username")->getBody();
+        // $this->user = json_decode($client->request('GET', "https://api.github.com/users/$username")->getBody());
+        $this->user =  json_decode($this->userData);
+    }
+
+    public function __get($func)
+    {
+      // return method_exists($this, $func) ? call_user_func(array($this, $func)) : $this->throwNoMethodException($func);
+      return method_exists($this, $func) ? call_user_func(array($this, $func)) : $this->useProperties($func);
+    }
+
+    public function useProperties($property)
+    {
+        return $this->hasProperty($property) ? $this->user->$property : $this->throwNoMethodException($property);
+    }
+
+    public function name()
+    {
+        return $this->user->name;
+    }
+
+    public function properties()
+    {
+      return array_keys( (array) $this->user);
+    }
+
+    public function numPublicRepos()
+    {
+        return $this->user->public_repos;
+    }
+
+    public function status()
+    {
+        return (new EvangelistStatus($this->user))->getStatus();
+    }
+
+    public function rank()
+    {
+        return $this->ranks[$this->level];
+    }
+
+    public function level()
+    {
+        if ($this->numPublicRepos() >= 0 && $this->numPublicRepos() <= 10) {
+        // if( in_array($this->numPublicRepos, range(1,10))) {
+            return $rank = 1;
+        }
+        if ($this->numPublicRepos() >= 11 && $this->numPublicRepos() <= 20) {
+        // if( in_array($this->numPublicRepos, range(1,10))) {
+            return $rank = 2;
+        }
+        if ($this->numPublicRepos() >= 21) {
+            return $rank = 3;
+        }
+    }
+
+    public function hasProperty($property)
+    {
+      return (bool) in_array($property, $this->properties());
+    }
+
+
+    private function throwNoMethodException($method)
+    {
+      // try {
+      //   throw new NoMethodException($method);
+      // } catch (NoMethodException $e) {
+      //   return $e->getMessage();
+      // }
+
+      throw new NoMethodException($method);
+    }
+
+}
+
+// $evan = new Evangelist('unicodeveloper');
+// $evan = new Evangelist('andela-celisha-wigwe');
+// $evan = new Evangelist('andela-joyebanji');
+
+// $evan = new Evangelist('andela-gjames');
+// $evan = new Evangelist('andela-badebiyi');
+// $evan = new Evangelist('andela-tolotin');
+// echo $evan->numPublicRepos();
+// echo $evan->name;
+// echo $evan->rank;
+// echo $evan->rank;
+// echo $evan->status;
+// echo $evan->company;
---
+++ tests/EvangelistStatusTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Elchroy\OSE\Evangelist;
+
+use Elchroy\OSE\EvangelistStatus;
+
+class EvangelistStatusTest extends PHPUnit_Framework_TestCase
+{
+    // public $ranks = array(
+    //         3 => '',
+    //     );
+    public function setUp()
+    {
+        $this->status = new EvangelistStatus('unicodeveloper');
+    }
+
+    public function testGetStatusFunctionWorks()
+    {
+        $this->assertEquals('Yeah, I crown you Senior Evangelist. Thanks for making the world a better place', $this->status->getStatus());
+    }
+}
