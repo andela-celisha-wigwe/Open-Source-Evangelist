8fd410e90de7eb0dd5671cf97490b9445125e594
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-02-28 07:52:03 +0100

    First commit
    This is the first commit to start the Open Source Evangelist Project.

---
+++ src/NoMethodException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Elchroy\OSE;
+
+class NoMethodException extends \Exception
+{
+    public $message;
+
+    public function __construct($method)
+    {
+        set_exception_handler(array($this, 'catchException'));
+        $this->message = "Method Error: '$method'. Probably the user does not have this feature.";
+    }
+
+    public function catchException()
+    {
+        echo $this->message;
+    }
+}
