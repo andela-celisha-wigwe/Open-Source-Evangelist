afe3ce927ffc7e80d898ccfdf966c4dd8f72f1ff
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-02-29 08:58:48 -0500

    [Chores] Apply fixes from StyleCI
    The arrangements and styling of codes are modified as suggested by fixes from StyleCI

--- src/NoMethodException.php
+++ src/NoMethodException.php
@@ -4,10 +4,10 @@ namespace Elchroy\OSE;
 
 class NoMethodException extends \Exception
 {
-    public function __construct($username ,$method)
+    public function __construct($username, $method)
     {
         $message = "Method Error: '$method' : $username may not have this feature.";
+
         return parent::__construct($message);
     }
-
 }
