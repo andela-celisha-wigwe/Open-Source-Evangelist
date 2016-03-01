86ca9ccc015f81cd11b50e29eb1b066410ec156e
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-03-01 13:12:10 +0100

    [Chores] Modify tests
    Add new tests for exceptions.

--- src/NoMethodException.php
+++ src/NoMethodException.php
@@ -7,6 +7,7 @@ namespace Elchroy\OSE;
  */
 class NoMethodException extends OSEException
 {
+
     /**
      * __construct First compose a custom message with the user name and the method being called.
      *
@@ -16,7 +17,6 @@ class NoMethodException extends OSEException
     public function __construct($username, $method)
     {
         $message = "Method Error: '$method' : $username may not have this feature."; // custom message.
-
         return parent::__construct($message); // Return an Exception using the parent class with the given message.
     }
 }
